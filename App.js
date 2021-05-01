e1baa22ba0231f49c8b30b755222ec9c791fc1fe
Electro255 <73359887+Electro255@users.noreply.github.com>
2021-05-01 15:34:32 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-native-tabs';
+import { createAppContainer } from 'react-navigation';
+import fb from './screens/fb';
+import insta from './screens/insta';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const TabNavigator = createBottomTabNavigator({
+  fb: {screens: fb},
+  insta: {screens: insta}
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
