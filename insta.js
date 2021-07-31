61778b55af859f309cd8ac1872a6a13ccd5babfb
Electro255 <73359887+Electro255@users.noreply.github.com>
2021-07-31 12:54:45 +0100

    Add files via upload

---
+++ insta.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-native-tabs';
+import { createAppContainer } from 'react-navigation';
+import fb from './screens/fb';
+import insta from './screens/insta';
